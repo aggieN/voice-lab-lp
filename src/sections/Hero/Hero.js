02cd3149eff0c543db24ebd5e38b74a505f754df
Agnieszka Niemiec <agnieszkaniemiec@MacBook-Pro-Agnieszka.local>
2023-05-24 11:26:09 +0200

    set up a project and create hero section

---
+++ src/sections/Hero/Hero.js
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import Wrapper from '@/components/Wrapper/Wrapper';
+
+import { alexandria } from "@/utils/fonts";
+
+import * as styles from './Hero.module.scss'
+
+
+const Hero = () => {
+  return (
+    <section>
+      <div className={styles.decor} />
+      <div className={styles.hero}>
+        <Wrapper>
+          <div className={styles.container}>
+            <div className={styles.box}>
+              <h1 className={alexandria.className}><span>Przestrzeń</span><span>kreatywnego</span><span>śpiewu</span></h1>
+              <button className={styles.buttonDesktop}>zapisz się na darmową konsultację</button>
+            </div>
+            <div className={styles.imageWrapper}>
+              <Image src="/aggie-hero-mobile.webp" priority={true} width={440} height={440} alt="Aggie" className={styles.imageMobile} />
+              <Image src="/aggie-hero.webp" priority={true} width={740} height={740} alt="Aggie" className={styles.imageDesktop} />
+            </div>
+            <button className={styles.buttonMobile}>zapisz się na darmową konsultację</button>
+          </div>
+        </Wrapper>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/utils/fonts.js
@@ -0,0 +1,6 @@
+import { Alexandria } from "next/font/google";
+
+export const alexandria = Alexandria({
+    subsets: ['latin-ext'],
+    weight: ['800']
+})
