c70edd4db1f324f26a2ecdeedbe16a90df27eee0
Agnieszka Niemiec <agnieszkaniemiec@MacBook-Pro-Agnieszka.local>
2023-06-14 12:19:39 +0200

    change mobile navigation to hamburger

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -28,7 +28,7 @@ const item = {
 
 const variants = {
   visible: { opacity: 1, y: 0, backgroundColor: "#FFF1D7" },
-  hidden: { opacity: 0, y: -25, backgroundColor: "#15183F" }
+  hidden: { opacity: 0, y: -25, backgroundColor: "transparent" }
 };
 
 const Navbar = () => {
@@ -60,49 +60,61 @@ const Navbar = () => {
 
   return (
 
-    <motion.nav
-      className={`${styles.navbar} ${alexandria.className}`}
-      variants={variants}
-      animate={hidden ? "hidden" : "visible"}
-      transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6 }}>
-      <Wrapper>
-        <div className={styles.menuDesktop}>
-          <Logo />
-          <ul className={`${styles.navDesktop} ${jura.className}`}>
-            <li><Link href="/omnie" >o mnie</Link></li>
-            <li><Link href="/lekcje-spiewu" >lekcje śpiewu</Link></li>
-            <li><Link href="/cennik" >cennik</Link></li>
-            <li><Link href="/kontakt" >kontakt</Link></li>
-          </ul>
-        </div>
+    <nav className={`${styles.navbar} ${alexandria.className}`}>
 
-        <div className={`${styles.star} ${styles.starLight} ${isMenuOpen ? styles.starExpanding : styles.starRotating}`} onClick={openMenu}>
-        </div>
-        <div className={`${styles.menuMobile} ${isMenuOpen && styles.menuMobileOpen}`}>
-          <motion.div
-            variants={item}
-            initial="hidden"
-            animate={isMenuOpen ? "show" : "hide"}
-            transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6, delay: 1 }}
-          >
-            <Logo onClick={() => setMenuOpen(false)} />
-          </motion.div>
-          <div className={`${styles.star} ${styles.starDark} ${isMenuOpen ? styles.starRotating : styles.starClosing}`} onClick={closeMenu}>
+      <motion.div className={styles.menuDesktopWrapper} variants={variants}
+        animate={hidden ? "hidden" : "visible"}
+        transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6 }}>
+        <Wrapper>
+          <div className={styles.menuDesktop} >
+            <Logo />
+            <ul className={`${styles.navDesktop} ${jura.className}`}>
+              <li><Link href="/omnie" >o mnie</Link></li>
+              <li><Link href="/lekcje-spiewu" >lekcje śpiewu</Link></li>
+              <li><Link href="/cennik" >cennik</Link></li>
+              <li><Link href="/kontakt" >kontakt</Link></li>
+            </ul>
           </div>
-          <motion.ul className={styles.navMobile} variants={container}
-            initial="hidden"
-            animate={isMenuOpen ? "show" : "hide"}>
-            <motion.li onClick={() => setMenuOpen(false)} variants={item}><Link href="/omnie" >o mnie</Link></motion.li>
-            <motion.li onClick={() => setMenuOpen(false)} variants={item}><Link href="/lekcje-spiewu" >lekcje śpiewu</Link></motion.li>
-            <motion.li onClick={() => setMenuOpen(false)} variants={item}><Link href="/cennik" >cennik</Link></motion.li>
-            <motion.li onClick={() => setMenuOpen(false)} variants={item}><Link href="/kontakt" >kontakt</Link></motion.li>
-          </motion.ul>
-          {/* <div className={styles.socials}>
+        </Wrapper>
+      </motion.div>
+
+      <div className={`${styles.star} ${styles.starLight} ${isMenuOpen ? styles.starExpanding : ''}`} onClick={openMenu}>
+        <div className={styles.hamburger}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div className={`${styles.menuMobile} ${isMenuOpen && styles.menuMobileOpen}`}>
+        <motion.div
+          variants={item}
+          initial="hidden"
+          animate={isMenuOpen ? "show" : "hide"}
+          transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6, delay: 1 }}
+        >
+          <Logo onClick={() => setMenuOpen(false)} />
+        </motion.div>
+        <div className={`${styles.star} ${styles.starDark} ${isMenuOpen ? '' : styles.starClosing}`} onClick={closeMenu}>
+          <div className={styles.hamburger}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <motion.ul className={styles.navMobile} variants={container}
+          initial="hidden"
+          animate={isMenuOpen ? "show" : "hide"}>
+          <motion.li onClick={() => setMenuOpen(false)} variants={item}><Link href="/omnie" >o mnie</Link></motion.li>
+          <motion.li onClick={() => setMenuOpen(false)} variants={item}><Link href="/lekcje-spiewu" >lekcje śpiewu</Link></motion.li>
+          <motion.li onClick={() => setMenuOpen(false)} variants={item}><Link href="/cennik" >cennik</Link></motion.li>
+          <motion.li onClick={() => setMenuOpen(false)} variants={item}><Link href="/kontakt" >kontakt</Link></motion.li>
+        </motion.ul>
+        {/* <div className={styles.socials}>
             <SocialIcons variant="light" />
           </div> */}
-        </div>
-      </Wrapper>
-    </motion.nav>
+      </div>
+
+    </nav>
   );
 };
 
